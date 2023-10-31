02e80fb2e4131d2a9adce060df928c9098c5be9a
rayhankobir-dev <rayhankobir793@gmail.com>
2023-10-31 12:07:20 +0600

    faq, service, features sections added

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,28 @@ import Testimonial from "./components/Testmonials";
 import Traffic from "./components/Traffic";
 import Services from "./components/Services";
 import BackToTop from "./services/BackToTop";
+import Faq from "./components/Faq";
+import Features from "./components/Features";
+import Teams from "./components/Teams";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <>
+    <Fragment>
       <Header />
       <Hero />
       <Services />
+      <Features />
       <Traffic />
-      <Blog />
       <Testimonial />
-      <Project />
+      <Teams />
       <Pricing />
+      <Faq />
+      <Blog />
+      <Project />
       <Footer />
       <BackToTop />
-    </>
+    </Fragment>
   );
 }
 
